706bc0061e5370fef6dc165de6296b9c6d22aa57
RoxyEmanuel26 <bimantara1997@yahoo.com>
2026-06-20 15:24:02 +0700

    hide floating telegram button and bump version to 2.0.8

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -4,7 +4,7 @@
  * untuk provider Adsterra & ExoClick, dan transparansi overlay di video player.
  */
 
-import ui from './ui.js?v=2.0.7';
+import ui from './ui.js?v=2.0.8';
 
 // ==========================================
 // HIJACK CLICK LISTENERS FOR POPUNDER BOUNDS
