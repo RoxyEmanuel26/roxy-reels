b67beb9d3df9376dbac260c758b3255c4b20adf4
RoxyEmanuel26 <bimantara1997@yahoo.com>
2026-07-28 00:25:51 +0700

    bump version to 2.8.3

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -4,7 +4,7 @@
  * serta fungsi lokalisasi statik & dinamis untuk seluruh antarmuka SPA.
  */
 
-import ui from './ui.js?v=2.8.2';
+import ui from './ui.js?v=2.8.3';
 
 // 13-language configuration with circular flag icons from /assets/pics
 export const LANGS = [
